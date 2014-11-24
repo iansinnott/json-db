2ea46b82da2a3cef2f0532a81214cd91fbba8077
Ian Sinnott <ian@iansinnott.com>
2014-11-25 00:26:21 +0800

    Add READ support

--- t.js
+++ t.js
@@ -7,16 +7,9 @@ var path = require('path');
 var DB = require('./lib');
 var Stuff = new DB('stuff');
 
-// console.log(_.last(require(Stuff._dbPath))); // debug
 Stuff.insert({ type: "House", purpose: "Raging" }, function(err, record) {
   if (err) throw err;
   console.log(Stuff.last());
   console.log(record);
   console.log("RAW DATabase: ", require(Stuff._dbPath)); // debug
 });
-
-// Stuff.save(function(err) {
-//   if (err) throw err;
-//   console.log("Async call completed"); // debug
-//   console.log(Stuff.records.length); // debug
-// });
--- test/crud.js
+++ test/crud.js
@@ -1,9 +1,8 @@
-var chai = require('chai'),
+var chai   = require('chai'),
     expect = chai.expect,
-    _ = require('lodash');
-
-var fs = require('fs'),
-    path = require('path');
+    path   = require('path'),
+    fs     = require('fs'),
+    _      = require('lodash');
 
 chai.should();
 
@@ -15,6 +14,22 @@ var DB     = require('../json-db'),
 function getJSON(filepath) {
   return JSON.parse(fs.readFileSync(filepath, { encoding: 'utf-8' }));
 }
+
+var testData = [
+  {
+    name: "James",
+    job: "Robocop",
+    status: "Boosh boosh boosh"
+  },{
+    name: "Carl Icahn",
+    job: "Rich Person",
+    status: "My name is strange."
+  },{
+    name: "J.K. Rowling",
+    job: "Author",
+    status: "My books are crazy popular."
+  }
+];
 
 describe('JSON DB CRUD', function() {
 
@@ -56,7 +71,31 @@ describe('JSON DB CRUD', function() {
     });
   });
 
-  it("Should READ JSON records.");
+  it("Should READ JSON records.", function(done) {
+
+    Things.insert(testData);
+    Things.save(function(err) {
+      if (err) throw err;
+      removeLocalCopy();
+    });
+
+    // Clear the local cache of the collection so that it can be fetched.
+    function removeLocalCopy() {
+      Things.records = [];
+      testRead();
+    }
+
+    function testRead() {
+      Things.count().should.equal(0);
+      Things.fetch(function(err) {
+        if (err) throw err;
+        Things.count().should.equal(3);
+        done();
+      });
+    }
+
+  });
+
   it("Should UPDATE JSON records.");
   it("Should DELETE JSON records.");
   it("Should RESET the DB.");
