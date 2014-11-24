34ae7ba71392db0aba106ad5977c33115ba546f6
Ian Sinnott <ian@iansinnott.com>
2014-11-25 01:47:30 +0800

    Finish up initial CRUD functionality

--- test/crud.js
+++ test/crud.js
@@ -36,7 +36,6 @@ describe('JSON DB CRUD', function() {
   beforeEach(function() {
     // beforeEach:Reset the database before each test.
     Things.reset();
-    // console.log("Database reset"); // debug
   });
 
   it("True should be true", function() { true.should.be.true; });
@@ -71,22 +70,29 @@ describe('JSON DB CRUD', function() {
     });
   });
 
-  it("Should READ JSON records.", function(done) {
+  it("Should be able to create multiple entries from an array", function() {
+    Things.count().should.equal(0);
+    Things.insert(testData, function(err) {
+      if (err) throw err;
+      Things.count().should.equal(testData.length);
+    });
+  });
 
+  it("Should READ JSON records.", function(done) {
     Things.insert(testData);
     Things.save(function(err) {
       if (err) throw err;
-      removeLocalCopy();
+      testRead();
     });
 
-    // Clear the local cache of the collection so that it can be fetched.
-    function removeLocalCopy() {
-      Things.records = [];
-      testRead();
-    }
-
     function testRead() {
+
+      // Clear the local cache of the collection so that it can be fetched.
+      Things.records = [];
+
       Things.count().should.equal(0);
+
+      // Now read the data from the db
       Things.fetch(function(err) {
         if (err) throw err;
         Things.count().should.equal(3);
@@ -96,8 +102,96 @@ describe('JSON DB CRUD', function() {
 
   });
 
-  it("Should UPDATE JSON records.");
-  it("Should DELETE JSON records.");
-  it("Should RESET the DB.");
+  it("Should UPDATE JSON records.", function(done) {
+    Things.insert(testData, afterInsertion);
+
+    function afterInsertion(err) {
+      if (err) throw err;
+
+      var entry = Things.first(),
+          originalName = entry.name,
+          update;
+
+      // Test local updating
+      update = Things.update(entry, { name: 'Face House' });
+      update.should.be.ok;
+      entry.name.should.not.equal(originalName);
+
+      // Can use IDs
+      update = Things.update(entry.id, { name: 'James' });
+      update.should.be.ok;
+      entry.name.should.equal(originalName);
+
+      // Pass a callback to trigger a save
+      Things.update(entry, { name: 'Spongebob' }, function(err) {
+        if (err) throw err;
+        Things.get(entry.id).name.should.equal('Spongebob');
+        Things.where({ name: 'James'}).should.be.empty;
+
+        // Reset the name locally to make sure that the update did not affect
+        // the DB
+        Things.update(entry, { name: 'James' });
+
+        Things.fetch(testFetch);
+      });
+
+      function testFetch(err) {
+        if (err) throw err;
+        Things.where({ name: 'James'}).should.be.empty;
+        Things.where({ name: 'Spongebob'}).length.should.equal(1);
+        done();
+      }
+    }
+
+  });
+
+  it("Should DELETE JSON records.", function(done) {
+    Things.insert(testData, afterInsertion);
+
+    function afterInsertion(err) {
+      if (err) throw err;
+
+      var entry = Things.first();
+
+      Things.count().should.equal(3);
+
+      Things.remove(entry);
+      Things.count().should.equal(2);
+      Things.fetch(afterFetch);
+    }
+
+    function afterFetch(err) {
+      if (err) throw err;
+
+      var entry = Things.first();
+
+      Things.count().should.equal(3);
+
+      Things.remove(entry, afterRemove);
+    }
+
+    function afterRemove(err) {
+      if (err) throw err;
+      Things.count().should.equal(2);
+      Things.fetchSync();
+      Things.count().should.equal(2);
+      done();
+    }
+
+  });
+
+  it("Should RESET the DB.", function(done) {
+    Things.insert(testData, afterInsertion);
+
+    function afterInsertion(err) {
+      if (err) throw err;
+      Things.count().should.equal(3);
+      Things.reset();
+      Things.count().should.equal(0);
+      Things.fetchSync();
+      Things.count().should.equal(0);
+      done();
+    }
+  });
 
 });
